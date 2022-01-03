2bdf39402f32cbb68f7a32505d9a1b8a8744c5fa
Callupe72 <paulleduc72@gmail.com>
2022-01-03 22:03:42 +0100

    AudioReactor

---
+++ Assets/Scripts/AudioReaction.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioReaction : MonoBehaviour
+{
+    [Space(20)]
+    public AudioSource audioSource;
+    public float updateStep = 0.1f;
+    public int sampleDataLength = 1024;
+
+    private float currentUpdateTime = 0f;
+
+    private float clipLoudness;
+    private float[] clipSampleData;
+
+    void Awake()
+    {
+        clipSampleData = new float[sampleDataLength];
+    }
+    void Update()
+    {
+        currentUpdateTime += Time.deltaTime;
+        if (currentUpdateTime >= updateStep)
+        {
+            currentUpdateTime = 0f;
+            audioSource.clip.GetData(clipSampleData, audioSource.timeSamples); //I read 1024 samples, which is about 80 ms on a 44khz stereo clip, beginning at the current sample position of the clip.
+            clipLoudness = 0f;
+            foreach (var sample in clipSampleData)
+            {
+                clipLoudness += Mathf.Abs(sample);
+                if (clipLoudness > 200)
+                {
+                    Debug.Log(clipLoudness);
+                }
+            }
+        }
+
+    }
+}
