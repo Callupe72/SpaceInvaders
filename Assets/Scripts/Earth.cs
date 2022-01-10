4edc0f1a6f8618fa5d7dfcb6a0ee44de35841db1
Callupe72 <paulleduc72@gmail.com>
2022-01-10 12:55:54 +0100

    Earth

---
+++ Assets/Scripts/Earth.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Earth : MonoBehaviour
+{
+    void Update()
+    {
+        transform.Rotate(-0.05f, 0, 0);
+    }
+}
