c36f738de791fab320c4ff5e3766bc6c3b90d3d2
Furyn <vincent_1999@hotmail.fr>
2022-01-12 09:54:49 +0100

    [FIX] Pause menu + Win Lose + Take dommage on shoot ennemy

--- Assets/Scripts/MenuPause.cs
+++ Assets/Scripts/MenuPause.cs
@@ -7,10 +7,16 @@ public class MenuPause : MonoBehaviour
 {
     [SerializeField] GameObject mainMenu = null;
     [SerializeField] GameObject creditPanel = null;
+    [SerializeField] GameObject optionPanel = null;
 
     public void Continue()
     {
         GameManager.Instance.Pause();
+    }
+    public void Option()
+    {
+        mainMenu.SetActive(false);
+        optionPanel.SetActive(true);
     }
 
     public void Credits()
@@ -23,6 +29,7 @@ public class MenuPause : MonoBehaviour
     {
         mainMenu.SetActive(true);
         creditPanel.SetActive(false);
+        optionPanel.SetActive(false);
     }
 
     public void Restart()
